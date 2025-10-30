d23c0c12ae6d05c6e22d6c87d2505a7873b05d13
alridho1201~ <raja.alridho@gmail.com.com>
2025-10-30 22:06:36 +0700

    add route /

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    return view('pages.home.index');
+});
+
 Route::get('/home', function () {
     return view('pages.home.index');
 });
